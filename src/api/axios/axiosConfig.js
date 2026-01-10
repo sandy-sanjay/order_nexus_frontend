ce67c73e70a861831369021287add9f5d0cb0bdc
sandy-sanjay <sandysanjay589@gmail.com>
2026-01-10 18:41:07 +0530

    frontend build config

--- src/api/axios/axiosConfig.js
+++ src/api/axios/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL
+,
 });
 
 export default axiosConfig;
